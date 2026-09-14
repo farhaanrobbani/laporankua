0b917d8de83b3c67d25b1a14d287b7194e7f47e5
Farhan Robbani <farhan@example.com>
2026-09-14 18:39:36 +0700

    style: rapatkan logo dan tulisan di kop surat (width 80→50, margin-left 64→30)

--- resources/views/reports/print.blade.php
+++ resources/views/reports/print.blade.php
@@ -103,8 +103,8 @@
                     @if (! empty($dataset['logo_kantor']))
                         <table class="w-full mb-1">
                             <tr>
-                                <td class="align-top" style="width: 80px;">
-                                    <img src="{{ asset('storage/' . $dataset['logo_kantor']) }}" alt="Logo" style="margin-left: 64px; max-height: 80px; max-width: 80px;" />
+                                <td class="align-top" style="width: 50px;">
+                                    <img src="{{ asset('storage/' . $dataset['logo_kantor']) }}" alt="Logo" style="margin-left: 30px; max-height: 80px; max-width: 50px;" />
                                 </td>
                                 <td class="text-center">
                                     <p class="font-bold uppercase" style="font-size: {{ $dataset['font_size_kop_kementerian'] ?? '12' }}px;">{{ $dataset['nama_kementerian'] ?? 'Kementerian Agama' }}</p>
