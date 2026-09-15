560b090c23b49db21726fb09a55a50fd5a8f12d3
Farhan Robbani <farhan@example.com>
2026-09-15 14:49:02 +0700

    fix: tambah min-h-[4.5rem] pada header container agar tinggi konsisten di semua halaman

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 
             @isset($header)
                 <header class="bg-white border-b border-gray-200">
-                    <div class="max-w-7xl mx-auto py-5 px-4 sm:px-6 lg:px-8">
+                    <div class="max-w-7xl mx-auto py-5 px-4 sm:px-6 lg:px-8 min-h-[4.5rem]">
                         {{ $header }}
                     </div>
                 </header>
