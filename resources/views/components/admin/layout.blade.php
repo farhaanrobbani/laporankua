5820eb35e28450ccfec96dedf6fcf66cd3b9bc24
Farhan Robbani <farhan@example.com>
2026-09-15 18:27:31 +0700

    Revert "fix: samakan posisi judul header di semua halaman dengan min-h-9 pada flex wrapper"
    This reverts commit b0858a52c2897588a1b806e54cd623ce4b0d844b.

--- resources/views/components/admin/layout.blade.php
+++ resources/views/components/admin/layout.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight min-h-9 flex items-center">{{ $header ?? 'Admin Panel' }}</h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ $header ?? 'Admin Panel' }}</h2>
     </x-slot>
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8">
