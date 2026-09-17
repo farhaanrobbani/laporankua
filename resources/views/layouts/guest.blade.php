aaed1898fd79e5d6053e96eaa83f8de60ca8a934
Farhan Robbani <farhan@example.com>
2026-09-17 11:27:10 +0700

    feat: tambah tombol dark mode toggle di halaman publik (login, forgot-password)

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -32,6 +32,9 @@
     </head>
     <body class="font-sans text-gray-900 dark:text-gray-100 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+            <div class="absolute top-4 right-4">
+                <x-theme-toggle />
+            </div>
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500 dark:text-gray-400" />
