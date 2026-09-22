7014de6fe0a863b154212a5b2d17bc5a71434198
Farhan Robbani <farhan@example.com>
2026-09-22 15:11:56 +0700

    fix: adjust Tahun-Model spacing in laporan_na header

--- resources/views/reports/print.blade.php
+++ resources/views/reports/print.blade.php
@@ -286,7 +286,7 @@
                     <div class="mb-3">
                         <h1 class="font-bold uppercase" style="font-size: 14px; text-align: left; padding-left: 40px;">BUKU STOK KHUSUS</h1>
                         <p style="font-size: 12px; padding-left: 40px;">Bulan : {{ strtoupper($bulanName) }}</p>
-                        <div style="font-size: 12px; padding-left: 40px; display: flex; justify-content: space-between;">
+                        <div style="font-size: 12px; padding-left: 40px; display: flex; gap: 20px;">
                             <span>Tahun : {{ $tahunName }}</span>
                             <span>Model : NA</span>
                         </div>
